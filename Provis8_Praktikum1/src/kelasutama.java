e9a9d18f585e1b8f813fa6c526d9175e8b4c2467
unknown <rifkyarya.it2@gmail.com>
2020-03-30 15:20:06 +0700

    kumpulin tugas

---
+++ Provis8_Praktikum1/src/kelasutama.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rifkyans
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        //System.out.println("Haii Dunia");
+        //menghidupkan objek atau instansiasi
+        //frameutama fu = new frameutama();
+        //fu.tampil("Program Pertama GUI");
+        
+        fu utama = new fu();
+        //utama.setTitle("Pemrograman Provis");
+        utama.setVisible(true);
+    }
+}
